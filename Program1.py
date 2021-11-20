1e657be89be0ea187ac0be0e895de58c227d8e94
unknown <you@example.com>
2021-11-20 23:32:27 +0800

    Program is functional only with unique numbers and if first number is the biggest

--- Program1.py
+++ Program1.py
@@ -10,4 +10,30 @@ def askFourNum():
     num4 = float(input("Enter 4th number: "))
     return num1, num2, num3, num4
 
-firstN, secondN, thirdN, FourthN = askFourNum()
+def highToLow(num1, num2, num3, num4):
+    if num1>num2 and num1>num3 and num1>num4:
+        if num2>num3 and num2>num4:
+            if num3>num4:
+                order = [num1, num2, num3, num4]
+                return order
+            else:
+                order = [num1, num2, num4, num3]
+                return order
+        elif num3>num2 and num3>num4:
+            if num2>num4:
+                order = [num1, num3, num2, num4]
+                return order
+            else:
+                order = [num1, num3, num4, num2]
+                return order
+        else:
+            if num3>num2:
+                order = [num1, num4, num3, num2]
+                return order
+            else:
+                order = [num1, num4, num2, num3]
+                return order
+    
+firstN, secondN, thirdN, fourthN = askFourNum()
+numOrder = highToLow(firstN, secondN, thirdN, fourthN)
+print(f" The order from highest to lowest is {numOrder}")
